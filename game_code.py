957213fa18583f4ddf47845bf59bb15d6db9d2f9
Colleen Stewart <collstewart30@gmail.com>
2022-07-12 22:16:23 -0400

    set up modules, directions

---
+++ game_code.py
@@ -0,0 +1,24 @@
+
+#   slay the vicious Nemean Lion Dictionary:
+Neamean_Lion_dict = {'health': 10, 'attack power': 10, 'attack names': ['attack_one', 'attack_two', 'attack_three', 'attack_four']}
+
+
+
+#   defeat the impossible nine-headed Lernaean Hydra Dictionary:
+guard_dog_dict = {'health': 10, 'attack power': 10, 'attack names': ['attack_one', 'attack_two', 'attack_three', 'attack_four']}
+
+
+
+#   capture the guard dog of the underworld—Cerberus Dictionary:
+underworld_Cerberus_dict = {'health': 10, 'attack power': 10, 'attack names': ['attack_one', 'attack_two', 'attack_three', 'attack_four']}
+
+
+#   Hercules Dictionary: 
+Hercules_dict = {'health': 10, 'attack power': 10, 'attack names': ['attack_one', 'attack_two', 'attack_three', 'attack_four']}
+
+
+#   menu to select Hercules attack
+#   foe's attack chosen at random
+#   
+#   an Attack() function that will terminate when Hercules or an enemy’s health reaches zero.
+#   RunGame() function to call my other functions in a logical order that will determine game flow.
---
+++ main.py
@@ -0,0 +1,3 @@
+#   RunGame() will go here
+
+#   RunGame() function to call my other functions in a logical order that will determine game flow.
