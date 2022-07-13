06f89e94b5303338b931092a6a0793e3fb301e05
Colleen Stewart <collstewart30@gmail.com>
2022-07-12 22:19:48 -0400

    updated notes/plan

--- game_code.py
+++ game_code.py
@@ -18,7 +18,14 @@ Hercules_dict = {'health': 10, 'attack power': 10, 'attack names': ['attack_one'
 
 
 #   menu to select Hercules attack
+#   menu example in day trip generator video
+#   also select attack power
+
 #   foe's attack chosen at random
+#   random from dictionary list. call key then random?
 #   
 #   an Attack() function that will terminate when Hercules or an enemy’s health reaches zero.
+#   counter -= 1
+#   if attack_one selected (for Hercules) against foe X, then decrease health
+
 #   RunGame() function to call my other functions in a logical order that will determine game flow.
